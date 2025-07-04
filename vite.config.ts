e1455c913fff36ab2a0dd6055a83674233aded03
premkakade2021 <premkakade2021@gmail.com>
2025-07-04 13:08:26 +0000

    Final push to Portfolio repo for deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
-// vite.config.ts or vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Portfolio/',// MUST match your GitHub repo name exactly
+  base: '/Portfolio/', // 🔥 MUST match your GitHub repo name
 });
